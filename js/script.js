6c35aea5ceb8dfa1c68608bbd2fabeb70ad088a9
melkiaskia <jonipeang661@gmail.com>
2025-10-27 22:54:01 +0700

    menambahkan fitur kirim form

---
+++ js/script.js
@@ -0,0 +1,8 @@
+// klik diluar sidebar untuk menghilangkan nav
+const hamburger = document.querySelector(".navbar");
+
+document.addEventListener("click", function (e) {
+  if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
+    navbarNav.classList.remove("active");
+  }
+});
